d02bba89813b80b634c39fe3fad3419f0ff8496f
HungH <Be Mai@DESKTOP-EVR59VF>
2020-12-15 23:06:13 +0700

    updated12.15

--- selenium/webDriver/Drop_Down.java
+++ selenium/webDriver/Drop_Down.java
@@ -1,9 +1,12 @@
 package webDriver;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
@@ -18,6 +21,8 @@ public class Drop_Down {
 	// Loai Custom: ul --> li/ div-->div ..
 	WebDriver driver;
 	Select select;
+	By job1 = By.id("job1");
+	String SelectedResult="";
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
@@ -27,10 +32,54 @@ public class Drop_Down {
 	}
 	
 	@Test
-	public void Testcase1() {
-		select = new Select(driver.findElement(By.id("job1")));
+	public void isNotMultibleSelected() {
+		select = new Select(driver.findElement(job1));
 		Assert.assertFalse(select.isMultiple());
-		
+		// index
+		select.selectByIndex(5);
+		SelectedResult = select.getFirstSelectedOption().getText();
+		Assert.assertEquals(SelectedResult, "Security Testing");
+		// visible text
+		Sleep(2);
+		select.selectByVisibleText("Security Testing");
+		SelectedResult = select.getFirstSelectedOption().getText();
+		Assert.assertEquals(SelectedResult, "Security Testing");
+		//value
+		select.selectByValue("security");
+		SelectedResult = select.getFirstSelectedOption().getText();
+		Assert.assertEquals(SelectedResult, "Security Testing");
+		// verify size
+		int sizeS = select.getOptions().size();
+		System.out.println(sizeS);
+		Assert.assertEquals(sizeS, 10);	
+	}
+	@Test
+	public void isMultibleSelected() {
+		select = new Select(driver.findElement(By.id("job2")));
+		Assert.assertTrue(select.isMultiple());
+		select.selectByVisibleText("Automation");
+		select.selectByVisibleText("Mobile");
+		select.selectByVisibleText("Desktop");
+		List <WebElement> ItemSelected = select.getAllSelectedOptions();
+		List <String> TextOfItemselected = new ArrayList<String>();
+		for (WebElement item: ItemSelected) {
+			TextOfItemselected.add(item.getText());
+		}
+		System.out.println(TextOfItemselected);
+		Assert.assertTrue(TextOfItemselected.contains("Automation"));
+		Assert.assertTrue(TextOfItemselected.contains("Mobile"));
+		Assert.assertTrue(TextOfItemselected.contains("Desktop"));
+		// deSelect 
+		//select.deselectAll();
+		//Sleep(2);
+		//Assert.assertEquals(TextOfItemselected, null);
+	}
+	public void Sleep(long tts) {
+		try {
+			Thread.sleep(tts * 1000);
+		} catch (InterruptedException e){
+			e.printStackTrace();
+		} 
 	}
 	
 	@AfterClass
